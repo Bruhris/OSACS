5766d27960751769c95a7d0c978732e27e6bab14
Boris <74333521+Bruhris@users.noreply.github.com>
2021-09-21 09:07:03 -0600

    added error check and commented

--- Exercise 2 - Day of the Week.py
+++ Exercise 2 - Day of the Week.py
@@ -10,10 +10,12 @@ days = ['monday', 'tuesday', 'wednesday',
         'thursday', 'friday', 'saturday', 'sunday']
 
 current_day = input('What day of the week is it right now? ')
+while current_day.lower() not in days:
+        current_day = input('What day of the week is it right now? ')
 wait_time = int(input('In how many days do you leave for your holiday? '))
 
 day_number = days.index(current_day.lower()) + 1
 
-day_leave = (day_number + wait_time) % 7 - 1
+day_leave = ((day_number + wait_time) % 7) - 1
 
 print('You leave on', days[day_leave])
--- Exercise 3 - Speech Analyzer.py
+++ Exercise 3 - Speech Analyzer.py
@@ -13,9 +13,9 @@ percent = 0
 
 base_text = input("Enter the text you want analyzed: ")
 
-new_text = base_text.translate(str.maketrans('', '', string.punctuation))
+new_text = base_text.translate(str.maketrans('', '', string.punctuation)) # Replaces all punctuation symbols with ' '
 
-text_words = new_text.split()
+text_words = new_text.split() # Puts all words into a list
 
 for i in text_words:
     if 'e' in i:
@@ -23,8 +23,8 @@ for i in text_words:
 
 num_words = len(text_words)
 
-if words_e != 0:
+if words_e != 0: 
     percent = words_e/num_words * 100
 
 print('Your text contains', num_words, 'of which',
-      words_e, '('+str(percent)+'%) contain an "e".')
+      words_e, '('+str(percent)+'%) contain the letter "e".')
