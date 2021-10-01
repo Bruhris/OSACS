05fef4af121f7b5f70de08e34e1c29f68009152e
Boris <74333521+Bruhris@users.noreply.github.com>
2021-10-01 16:52:56 -0600

    Commented for the Exercise

--- Exercise 2 - Date Guessing Game.py
+++ Exercise 2 - Date Guessing Game.py
@@ -8,34 +8,35 @@ def calendar(month_names, num_days_in_month):
    if len(month_names) != len(num_days_in_month):
        return []
 
-   dates = []
+   dates = [] # Creates an empty list and a counter variable idx
    idx = 0
 
-   while idx < len(month_names):
-       for date in range(1, num_days_in_month[idx] + 1):
-           dates.append(month_names[idx] + " " + str(date))
-       idx = idx + 1
-   return dates
+   while idx < len(month_names): # Goes through every month in the list month_names
+       for date in range(1, num_days_in_month[idx] + 1): # Gets every day in that month from 1 to the last day
+           dates.append(month_names[idx] + " " + str(date)) # Adds the string with the month and date to the dates list
+       idx = idx + 1 # Adds 1 to idx to go to next month until it reaches the end
+   return dates # Returns the list of dates
 
 
-first = calendar(month_names, num_days_in_month)
+first = calendar(month_names, num_days_in_month) # Creates first list that contains the entire years worth of dates
 
 
 def guess_game(first = calendar(month_names, num_days_in_month)):
-   mid = len(first)//2
+   mid = len(first)//2 # Gets the middle index of the list filled with the dates
 
-   val = is_earlier(first[mid])
+   val = is_earlier(first[mid]) # Checks if the guessed date is earlier, later or equal to the players guessed day
 
    if val == 1:
-       return guess_game(first[:mid + 1])
+       return guess_game(first[:mid + 1]) # If the date is earlier, will get all the dates that are before the index of the guessed date in list and run guess_game again with list split in half
    elif val == 2:
-       return guess_game(first[mid - 1:])
+       return guess_game(first[mid - 1:]) # If the date is later, will get all the dates that are before the index of the guessed date in list and runs guess_game again with list split in half
    else:
-       return first[mid]
+       return first[mid] # If the date is equal, returns the value of the date and ends the program
 
 
 def is_earlier(guess = 10):
-   return int(input("{}: 1 - earlier, 2 - later, 3 - equal?: ".format(guess))) # Asks the user for 
+   return int(input("{}: 1 - earlier, 2 - later, 3 - equal?: ".format(guess))) # Puts the date that the computer has guessed and asks the user
+                                                                               # if the date is earlier, later or equal
 
 
 if __name__ == '__main__':
