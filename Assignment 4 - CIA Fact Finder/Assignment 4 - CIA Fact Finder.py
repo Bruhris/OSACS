7c3fcead3d8fd5e70aefd397060007375561a11c
Boris <74333521+Bruhris@users.noreply.github.com>
2021-11-04 16:12:41 -0600

    Update Assignment 4 - CIA Fact Finder.py

--- Assignment 4 - CIA Fact Finder/Assignment 4 - CIA Fact Finder.py
+++ Assignment 4 - CIA Fact Finder/Assignment 4 - CIA Fact Finder.py
@@ -15,9 +15,48 @@ def getFile(file_type):
     txt_file = open(file_type, "r+")
     return txt_file
 
+def country_check(file, information):
+    with open(information, "r") as chicken:
+        lines = chicken.readlines()
+    data = []
+    for line in file:
+        temp = r.sub(" ",line).split()
+        data.append(temp)
+    print(data)
+    while True:
+        choice = input("Do you want to check a country? (Y or N) ")
+        while choice.lower() != 'y' and choice.lower() != 'n':
+            print("That is an invalid input")
+            choice = input("Do you want to check a country? (Y or N) ")
+        if choice == 'y':
+            country = input("Enter the name of the country that you want to check: ").title()
+            country_include = False
+            for i in data:
+                if country in i:
+                    print(lines[data.index(i)], end = "")
+                    country_include = True
+                    break
+            if country_include == False:
+                print("Sorry the country you were looking for wasn't in the GDP file")
+            else:
+                break
+        while True:
+            add_country = input("Do you want to add a countries information to the file? (Y or N)")
+            while add_country.lower() != 'y' and add_country.lower() != 'n':
+                print("That is an invalid input")
+                add_country = input("Do you want to check a country? (Y or N) ")
+            if add_country == 'y':
+                c_name = input("Enter the name of the country")
+                c_values = input("Enter the values that you want to add to data (Only numbers)")
+                c_region = input("Enter the region that your country is in")
+                add_text = "\n"+c_name+" "+c_values+" "+c_region
+                f = open(file, "a")
+                f.write(add_text)
+            else:
+                break
+    
+
 def main():
-    gdp = []
-    birth_rate = []
     directory = input("What is the directory where the text files are stored? ")
     if os.path.isdir(directory) == False:
         print("That is not a reachable/valid directory!")
@@ -38,22 +77,8 @@ def main():
     else:
         type = 'unemployment.txt'
     txt_file = getFile(type)
-    for line in txt_file:
-        temp = r.sub(" ",line).split()
-        gdp.append(temp)
-    country = input("What country do you want to check? ")
-    country_include = False
-    for i in gdp:
-        if country in i:
-            print(i[0],i[1],i[2])
-            country_include = True
-            break
-    if country_include == False:
-        print("Sorry the country you were looking for wasn't in the GDP file")
-        add_data = input("Do you want to add a country with their GDP and region to the file? (Y or N)")
-        
 
-    txt_file.close()
+    country_check(txt_file, type)
     
     
     
