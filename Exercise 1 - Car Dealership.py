df80b35887233021fc3c529f84222369a0c64428
Boris <74333521+Bruhris@users.noreply.github.com>
2021-11-09 23:57:25 -0700

    Added initial exercise files to module

---
+++ Exercise 1 - Car Dealership.py
@@ -0,0 +1,28 @@
+class Car:
+    def __init__(self, make, model, year, odometer, colour, price):
+        self.make = make
+        self.year = year
+        self.model = model
+        self.odometer = odometer
+        self.colour = colour
+        self.price = price
+    def car_information(self):
+        print()
+        print("The make of the car is",self.make)
+        print("The model of the car is",self.model)
+        print("The year the car was made was",str(self.year))
+        print("The type of odometer the car has is",self.odometer)
+        print("The color of the car is",self.colour)
+        print("The price of the car is $"+str(self.price))
+
+car1 = Car("Lexus", "LC 500", "2001", "Mechanical", "Red", "1000")
+car2 = Car("Ford", "Water Bottle", "1991", "Digital", "Blue", "1500")
+car3 = Car("BMW", "Sauce", "1200", "Mechanical", "Yellow", "1040")
+car4 = Car("Chevrolet", "Guacamole", "1945", "Mechanical", "Green", "4200")
+car5 = Car("Mitsubishi", "Nuggets", "1835", "Mechanical", "Cyan", "6031")
+car6 = Car("Tesla", "Fruit", "5014", "Mechanical", "Magenta", "1456")
+
+cars = [car1,car2,car3,car4,car5,car6]
+
+for i in cars:
+    i.car_information()
