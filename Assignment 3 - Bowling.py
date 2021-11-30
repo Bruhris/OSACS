9ab94f64ec9ea3e832e6eb21f9daed96b7f20543
Boris <74333521+Bruhris@users.noreply.github.com>
2021-11-30 10:22:15 -0700

    Added files

---
+++ Assignment 3 - Bowling.py
@@ -0,0 +1,11 @@
+class BowlingBall:
+    def __init__(self):
+        print("hi")
+
+
+def main():
+    print("Welcome to Boris' Bowling Game!~")
+    playerBall = BowlingBall()
+
+if __name__ == "__main__":
+    main()
--- Exercise 2 - Ducklings.py
+++ Exercise 2 - Ducklings.py
@@ -44,46 +44,45 @@ def main():
     ducklingLanes = [None] * (WIDTH // DUCKLING_WIDTH)
 
     while True:  
-        for laneNum, ducklingObj in enumerate(ducklingLanes):
+        for laneNum, ducklingObj in enumerate(ducklingLanes): # Goes through ducklingLanes and gets the laneNum (index) and the ducklingObj (element)
             
-            if (ducklingObj == None and random.random() <= DENSITY):
+            if (ducklingObj == None and random.random() <= DENSITY): # If element is None and a random value is less than or equal to DENSITY
                     
-                    ducklingObj = Duckling()
+                    ducklingObj = Duckling()                        # Create new ducklingObj and assign it to current element
                     ducklingLanes[laneNum] = ducklingObj
 
-            if ducklingObj != None:
+            if ducklingObj != None: # If ducklingObj is not equal to none
                 
                 # Displays part of the duckling then selects new body part
                 print(ducklingObj.getNextBodyPart(), end='')
-                    
+                # If there are no more parts of the duckling to display, reassign element to none
                 if ducklingObj.partToDisplayNext == None:
                     ducklingLanes[laneNum] = None
             else:
-   
-                print(' ' * DUCKLING_WIDTH, end='')
+                print(' ' * DUCKLING_WIDTH, end='') # If the ducklingObj is none and random value is more then DENSITY, finishing drawing the current duck
 
         print()  
-        sys.stdout.flush()  
+        sys.stdout.flush() 
         time.sleep(PAUSE)
 
 # Create Duckling class
 class Duckling:
     # Constructor of Duckling that gives random attributes to new duckling
     def __init__(self):
-
+        # Assigns a random attributes to duckling
         self.direction = random.choice([LEFT, RIGHT])
         self.body = random.choice([CHUBBY, VERY_CHUBBY])
         self.mouth = random.choice([OPEN, CLOSED])
         self.wing = random.choice([OUT, UP, DOWN])
-
+        
         if self.body == CHUBBY:
             self.eyes = BEADY
         else:
             self.eyes = random.choice([BEADY, WIDE, HAPPY, ALOOF])
-
+        # First part to display is HEAD
         self.partToDisplayNext = HEAD
 
-    # Creates head duckling as string based on instance variables and returns it
+    # Creates head duckling as string based on instance variables and returns string
     def getHeadStr(self):
         headStr = ''
         if self.direction == LEFT:
@@ -110,7 +109,6 @@ class Duckling:
         if self.direction == RIGHT:
             headStr += ' ('  
 
-
             if self.eyes == BEADY and self.body == CHUBBY:
                 headStr += '"'
             elif self.eyes == BEADY and self.body == VERY_CHUBBY:
@@ -130,20 +128,18 @@ class Duckling:
 
         if self.body == CHUBBY:
 
-
             headStr += ' '
 
         return headStr
-    # Creates body of duckling as string based on instance variables and returns it
+
+    # Creates body of duckling as string based on instance variables and returns string
     def getBodyStr(self):
         bodyStr = '('  
         if self.direction == LEFT:
-
             if self.body == CHUBBY:
                 bodyStr += ' '
             elif self.body == VERY_CHUBBY:
                 bodyStr += '  '
-
 
             if self.wing == OUT:
                 bodyStr += '>'
@@ -178,21 +174,21 @@ class Duckling:
             return ' ^^  '
         elif self.body == VERY_CHUBBY:
             return ' ^ ^ '
+
     # Checks which body part must be displayed next and returns the body part. If there is nobody parts left, return none
     def getNextBodyPart(self):
-        if self.partToDisplayNext == HEAD:
+        if self.partToDisplayNext == HEAD: # If displayed HEAD already, display BODY next
             self.partToDisplayNext = BODY
-            return self.getHeadStr()
-        elif self.partToDisplayNext == BODY:
+            return self.getHeadStr()    
+        elif self.partToDisplayNext == BODY: # If displayed BODY already, display FEET next
             self.partToDisplayNext = FEET
             return self.getBodyStr()
-        elif self.partToDisplayNext == FEET:
+        elif self.partToDisplayNext == FEET: # If displayed FEET already, no more to display so return NONE
             self.partToDisplayNext = None
             return self.getFeetStr()
 
 
 
-# If this program was run (instead of imported), run the game:
 if __name__ == '__main__':
     try:
         main()
