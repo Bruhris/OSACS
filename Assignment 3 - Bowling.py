b09487c084c9cee50bf17a86fc026bd9fba48673
Boris <74333521+Bruhris@users.noreply.github.com>
2021-12-03 15:15:42 -0700

    Update Assignment 3 - Bowling.py

--- Assignment 3 - Bowling.py
+++ Assignment 3 - Bowling.py
@@ -1,13 +1,19 @@
 import random
 
-class Bowling:
+class Player:
     def __init__(self):
         self.score = 0
+        self.pins = 10
+
+
+class Bowling:
+    def __init__(self):
+        pass
 
     def get_frames(self):
         while True:
             try:
-                frames = input("How many frames do you want to play against the computer? ")
+                frames = input("How many frames do you want to play against your opponent(s)? ")
                 self.frames = frames
                 break
             except TypeError:
@@ -25,13 +31,24 @@ class Bowling:
     def get_pins(self):
         while True:
             try:
-                pins = int(input("How many pins do you in your game? "))
+                pins = int(input("How many pins do you want in your game? "))
                 self.pins = pins
                 break
             except TypeError:
                 print("That is not a valid input!")
+    def get_players(self):
+        while True:
+            try:
+                no_players = int(input("How many players do you want in your game? "))
+                self.player_num = no_players
+                break
+            except TypeError:
+                print("That is not a valid input!")
 
-    def throw(self):
+    def set_players(self):
+        return self.player_num
+
+    '''def throw(self, player):
         self.pins_hit = random.randint(0,self.pins)
         while self.pins_hit > self.pins:
             self.pins_hit = random.randint(0,self.pins)
@@ -43,7 +60,7 @@ class Bowling:
         print(f"You hit {self.pins_hit} pins!")
         print(f"You scored {self.got_Score} points!")
         print(f"Your total score is {self.score}!")
-        print(f"There are {self.pins} remaining")
+        print(f"There are {self.pins} remaining")'''
             
         
 
@@ -55,8 +72,16 @@ def main():
     BowlingGame.get_frames()
     BowlingGame.get_throws()
     BowlingGame.get_pins()
+    BowlingGame.get_players()
+    players = {}
 
-    BowlingGame.throw()
+    for i in range(1, BowlingGame.set_players()+1):
+        name = "player_{}".format(i)
+        players[name] = players.get(name, Player())
+
+
+    
+
 
 if __name__ == "__main__":
     main()
