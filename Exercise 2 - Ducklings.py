e80c3155e6f8716520fe39a67f49c6871831baa7
Boris <74333521+Bruhris@users.noreply.github.com>
2021-12-07 17:04:14 -0700

    Update Exercise 2 - Ducklings.py

--- Exercise 2 - Ducklings.py
+++ Exercise 2 - Ducklings.py
@@ -42,39 +42,39 @@ def main():
     time.sleep(2)
 
     ducklingLanes = [None] * (WIDTH // DUCKLING_WIDTH)
-
+# Loop to continue creating ducklings in text space
     while True:  
         for laneNum, ducklingObj in enumerate(ducklingLanes): # Goes through ducklingLanes and gets the laneNum (index) and the ducklingObj (element)
-            
+           
             if (ducklingObj == None and random.random() <= DENSITY): # If element is None and a random value is less than or equal to DENSITY
-                    
-                    ducklingObj = Duckling()                        # Create new ducklingObj and assign it to current element
+                   
+                    ducklingObj = Duckling()                       # Create new Duckling object and assign it to current element
                     ducklingLanes[laneNum] = ducklingObj
 
             if ducklingObj != None: # If ducklingObj is not equal to none
-                
+               
                 # Displays part of the duckling then selects new body part
                 print(ducklingObj.getNextBodyPart(), end='')
                 # If there are no more parts of the duckling to display, reassign element to none
                 if ducklingObj.partToDisplayNext == None:
                     ducklingLanes[laneNum] = None
             else:
-                print(' ' * DUCKLING_WIDTH, end='') # If the ducklingObj is none and random value is more then DENSITY, finishing drawing the current duck
+                print(' ' * DUCKLING_WIDTH, end='') # If the ducklingObj is none and random value is more then DENSITY, finish drawing the current duck
 
         print()  
-        sys.stdout.flush() 
+        sys.stdout.flush()  
         time.sleep(PAUSE)
 
 # Create Duckling class
 class Duckling:
     # Constructor of Duckling that gives random attributes to new duckling
     def __init__(self):
-        # Assigns a random attributes to duckling
+        # Assigns a random attribute to duckling
         self.direction = random.choice([LEFT, RIGHT])
         self.body = random.choice([CHUBBY, VERY_CHUBBY])
         self.mouth = random.choice([OPEN, CLOSED])
         self.wing = random.choice([OUT, UP, DOWN])
-        
+       
         if self.body == CHUBBY:
             self.eyes = BEADY
         else:
@@ -82,7 +82,7 @@ class Duckling:
         # First part to display is HEAD
         self.partToDisplayNext = HEAD
 
-    # Creates head duckling as string based on instance variables and returns string
+    # Creates head duckling as string based on instance variables and returns string to display
     def getHeadStr(self):
         headStr = ''
         if self.direction == LEFT:
@@ -92,7 +92,7 @@ class Duckling:
             elif self.mouth == CLOSED:
                 headStr += '='
 
-
+		
             if self.eyes == BEADY and self.body == CHUBBY:
                 headStr += '"'
             elif self.eyes == BEADY and self.body == VERY_CHUBBY:
@@ -104,7 +104,7 @@ class Duckling:
             elif self.eyes == ALOOF:
                 headStr += '``'
 
-            headStr += ') ' 
+            headStr += ') '
 
         if self.direction == RIGHT:
             headStr += ' ('  
@@ -120,7 +120,7 @@ class Duckling:
             elif self.eyes == ALOOF:
                 headStr += '``'
 
-            # Get the mouth:
+            
             if self.mouth == OPEN:
                 headStr += '<'
             elif self.mouth == CLOSED:
@@ -131,8 +131,7 @@ class Duckling:
             headStr += ' '
 
         return headStr
-
-    # Creates body of duckling as string based on instance variables and returns string
+    # Creates body of duckling as string based on instance variables and returns string to display
     def getBodyStr(self):
         bodyStr = '('  
         if self.direction == LEFT:
@@ -168,7 +167,7 @@ class Duckling:
             bodyStr += ' '
 
         return bodyStr
-    # Creates feet of duckling as string based on instance variables and returns it
+    # Creates feet of duckling as string based on instance variables and returns it to display
     def getFeetStr(self):
         if self.body == CHUBBY:
             return ' ^^  '
@@ -177,7 +176,7 @@ class Duckling:
 
     # Checks which body part must be displayed next and returns the body part. If there is nobody parts left, return none
     def getNextBodyPart(self):
-        if self.partToDisplayNext == HEAD: # If displayed HEAD already, display BODY next
+        if self.partToDisplayNext == HEAD: # If going to display HEAD already, display BODY next
             self.partToDisplayNext = BODY
             return self.getHeadStr()    
         elif self.partToDisplayNext == BODY: # If displayed BODY already, display FEET next
@@ -186,11 +185,8 @@ class Duckling:
         elif self.partToDisplayNext == FEET: # If displayed FEET already, no more to display so return NONE
             self.partToDisplayNext = None
             return self.getFeetStr()
-
-
-
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        sys.exit()  # When Ctrl-C is pressed, end the program.
+        sys.exit()
